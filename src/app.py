df6c62883d91de2622e6fbae39cfb550fe2a2bf4
Bender0x1337 <peteleadsyourcompany@gmail.com>
2026-07-10 22:32:36 +0000

    changes

--- src/app.py
+++ src/app.py
@@ -38,7 +38,7 @@ activities = {
         "schedule": "Mondays, Wednesdays, Fridays, 2:00 PM - 3:00 PM",
         "max_participants": 30,
         "participants": ["john@mergington.edu", "olivia@mergington.edu"]
-    },
+,
     "Basketball Team": {
         "description": "Competitive basketball games and practice drills",
         "schedule": "Mondays and Wednesdays, 4:00 PM - 5:30 PM",
@@ -75,6 +75,7 @@ activities = {
         "max_participants": 18,
         "participants": ["christopher@mergington.edu"]
     }
+}    }
 }
 
 
